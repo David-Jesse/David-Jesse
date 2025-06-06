6a86a14a2cd4f7287aa4471a6d577d61f2cc7e64
David-Jesse <djesse351@gmail.com>
2025-06-06 15:16:07 +0100

    made changes to the hero button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -91,7 +91,8 @@ const Hero = () => {
                         initial={{ y: 30, opacity: 0 }}
                         whileInView={{ y: 0, opacity: 1 }}
                         transition={{ duration: 0.6, delay: 0.7 }}
-                        href="#projects" className='cursor-pointer w-[50%] sm:w-auto px-8 py-3 sm:px-10 sm:py-4 border-transparent border rounded-full bg-black text-white hover:bg-gray-800 transition-all duration-300 font-medium text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center gap-2' onClick={() => smoothScrollTo('projects')}>
+                        href="#projects" 
+                        className='cursor-pointer w-[50%] sm:w-auto px-8 py-3 sm:px-10 sm:py-4 border-transparent border rounded-full bg-black text-white hover:bg-gray-800 transition-all duration-300 font-medium text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center gap-2' onClick={() => smoothScrollTo('projects')}>
                         EXPLORE PROJECTS
                     </motion.button>
 
@@ -99,7 +100,8 @@ const Hero = () => {
                         initial={{ y: 30, opacity: 0 }}
                         whileInView={{ y: 0, opacity: 1 }}
                         transition={{ duration: 0.6, delay: 0.7}}
-                        href="/david-jesse-resume.pdf" download="David-Jesse-Resume" target="_blank" rel="noopener noreferrer" className='w-[44%] sm:w-auto px-8 py-3 sm:px-10 sm:py-4 border rounded-full border-gray-500 duration-300 flex items-center sm:text-base focus:outline-none focus:ring-offset-2 focus:ring-gray-400 gap-2 hover:bg-gray-50'>My Resume <Image src={assets.download_icon} alt='right arrow' className='w-4' />
+                        href="/david-jesse-resume.pdf" download="David-Jesse-Resume" target="_blank" rel="noopener noreferrer" 
+                        className='w-[50%] sm:w-auto px-8 py-3 sm:px-10 sm:py-4 border rounded-full border-gray-500 duration-300 flex items-center justify-center sm:text-base focus:outline-none focus:ring-offset-2 focus:ring-gray-400 gap-2 hover:bg-gray-50'>My Resume <Image src={assets.download_icon} alt='right arrow' className='w-4' />
                     </motion.a>
                 </div>
             </div>
